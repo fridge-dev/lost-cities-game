d9de52fd5020eec35b994372b3486a4acee351bd
Alec Von Arx <alecva@amazon.com>
2020-03-01 00:50:46 -0800

    Move API impl layer into its own file - now api/src/lib.rs contains only the API definition! cool

---
+++ src/crates/api/src/handler.rs
@@ -0,0 +1,40 @@
+use crate::GameApi;
+use crate::types::{GameMetadata, GameError, GameState, Play};
+
+pub struct GameApiHandler;
+
+impl GameApi for GameApiHandler {
+    fn create_game(&self) -> Result<GameMetadata, GameError> {
+        Ok(create_game())
+    }
+
+    fn describe_game(&self, game_id: &str) -> Result<GameMetadata, GameError> {
+        Ok(describe_game(game_id))
+    }
+
+    fn get_game_state(&self, game_id: &str) -> Result<GameState, GameError> {
+        unimplemented!()
+    }
+
+    fn play_card(&self, play: Play) -> Result<(), GameError> {
+        unimplemented!()
+    }
+}
+
+/// Create game, return game ID
+fn create_game() -> GameMetadata {
+    GameMetadata::new(
+        "g-id".to_owned(),
+        "p1p1".to_owned(),
+        "p2p2p2".to_owned(),
+    )
+}
+
+/// Note to future self: something like this could exist.
+fn describe_game(game_id: &str) -> GameMetadata {
+    GameMetadata::new(
+        game_id.to_owned(),
+        "p1p1".to_owned(),
+        "p2p2p2".to_owned(),
+    )
+}
--- src/crates/api/src/lib.rs
+++ src/crates/api/src/lib.rs
@@ -3,60 +3,31 @@
 #![allow(unused_variables)]
 
 use crate::types::{GameError, GameMetadata, GameState, Play};
+use crate::handler::GameApiHandler;
 
 pub mod types;
+mod handler;
 
-/// Maybe this is a little too OOP? oh well...
-/// But maybe I can move all of the impl logic into another file. that'd be cool!
+/// Maybe this is a little too OOP? this is a learning experiment.
 pub trait GameApi {
+
+    /// TODO awesome doc
     fn create_game(&self) -> Result<GameMetadata, GameError>;
+
+    /// TODO awesome doc
     fn describe_game(&self, game_id: &str) -> Result<GameMetadata, GameError>;
+
+    /// TODO awesome doc
     fn get_game_state(&self, game_id: &str) -> Result<GameState, GameError>;
+
+    /// TODO awesome doc
     fn play_card(&self, play: Play) -> Result<(), GameError>;
 }
 
 // Does this mean every call from main to API will incur the cost of a v-lookup table query?
 // Consider removing this interface. See https://stackoverflow.com/a/27570064.
-pub fn new_api() -> Box<dyn GameApi> {
+pub fn new_game_api() -> Box<dyn GameApi> {
     Box::new(GameApiHandler)
-}
-
-struct GameApiHandler;
-
-impl GameApi for GameApiHandler {
-    fn create_game(&self) -> Result<GameMetadata, GameError> {
-        Ok(create_game())
-    }
-
-    fn describe_game(&self, game_id: &str) -> Result<GameMetadata, GameError> {
-        Ok(describe_game(game_id))
-    }
-
-    fn get_game_state(&self, game_id: &str) -> Result<GameState, GameError> {
-        unimplemented!()
-    }
-
-    fn play_card(&self, play: Play) -> Result<(), GameError> {
-        unimplemented!()
-    }
-}
-
-/// Create game, return game ID
-fn create_game() -> GameMetadata {
-    GameMetadata::new(
-        "g-id".to_owned(),
-        "p1p1".to_owned(),
-        "p2p2p2".to_owned(),
-    )
-}
-
-/// Note to future self: something like this could exist.
-fn describe_game(game_id: &str) -> GameMetadata {
-    GameMetadata::new(
-        game_id.to_owned(),
-        "p1p1".to_owned(),
-        "p2p2p2".to_owned(),
-    )
 }
 
 #[cfg(test)]
